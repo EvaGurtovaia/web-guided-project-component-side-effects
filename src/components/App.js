5b057a440f852b592d30f354346997d9e2cec481
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-01-23 18:25:25 -0600

    Rendering the list of friends

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 // 👉 TASK 1 - import the axios lib from node_modules
-
+import axios from "axios";
 // 👉 TASK 2 - import the contants from constants/index.js
+import { BASE_URL, API_KEY } from "../constants/index";
 
-import Details from './Details'
-import Friend from './Friend';
+import Details from "./Details";
+import Friend from "./Friend";
 
 export default function App() {
-  const [friends, setFriends] = useState([])
-  const [currentFriendId, setCurrentFriendId] = useState(null)
+    const [friends, setFriends] = useState([]);
+    const [currentFriendId, setCurrentFriendId] = useState(null);
+    const [error, setError] = useState(null);
 
-  const openDetails = id => {
-    setCurrentFriendId(id)
-  }
+    const openDetails = (id) => {
+        setCurrentFriendId(id);
+    };
 
-  const closeDetails = () => {
-    setCurrentFriendId(null)
-  }
+    const closeDetails = () => {
+        setCurrentFriendId(null);
+    };
 
-  // 👉 TASK 3 - make an effect that runs after FIRST DOM surgery
-  // caused by the first render only. You'll need `useEffect` from React.
-  // The effect should consist of a call to the API using axios.
-  // On success, set the array of friend objects from the API into state.
+    // 👉 TASK 3 - make an effect that runs after FIRST DOM surgery
+    // caused by the first render only. You'll need `useEffect` from React.
+    // The effect should consist of a call to the API using axios.
+    // On success, set the array of friend objects from the API into state.
+    useEffect(() => {
+        axios
+            .get(`${BASE_URL}/friends?api_key=${API_KEY}`)
+            .then((response) => {
+                setFriends(response.data);
+            })
+            .catch((error) => {
+                setError(error);
+            });
+    }, []);
+    //if (error) return `Error: ${error.message}`;
+    //if (!friends) return "No post!";
 
-  return (
-    <div className='container'>
-      <h1>Some of my friends:</h1>
-      {/* start by mapping over the friends array...*/}
-      {
-        currentFriendId && <Details friendId={currentFriendId} close={closeDetails} />
-      }
-    </div>
-  )
+    return (
+        <div className="container">
+            <h1>Some of my friends:</h1>
+            {friends.map((friend) => (
+                <Friend key={friend.id} info={friend} />
+            ))}
+            {currentFriendId && (
+                <Details friendId={currentFriendId} close={closeDetails} />
+            )}
+        </div>
+    );
 }
